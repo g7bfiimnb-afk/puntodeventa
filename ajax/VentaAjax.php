85ef5bbd83bfa0ab172df07f7bd27c0c617f5967
victor <g7bfiimnb@gmail.com>
2026-03-23 12:45:49 -0800

    cobrar producto2

--- ajax/VentaAjax.php
+++ ajax/VentaAjax.php
@@ -4,22 +4,27 @@ require_once "../controladores/VentaControlador.php";
 
 $insVenta = new VentaControlador();
 
-// 1. Acción para buscar un producto (Cuando usas la lupa o Enter)
+// 1. Buscar producto (Lupa o Enter)
 if(isset($_POST['buscar_codigo'])) {
-    echo $insVenta->buscar_producto_venta_controlador($_POST['buscar_codigo']);
-    exit(); // Importante para detener la ejecución aquí
-}
-
-// 2. Acción para finalizar la venta (Cuando das clic en Cobrar)
-if(isset($_POST['productos_venta'])) {
-    echo $insVenta->guardar_venta_controlador();
+    ob_start(); // Inicia un "escudo" para atrapar errores de texto
+    $respuesta = $insVenta->buscar_producto_venta_controlador($_POST['buscar_codigo']);
+    ob_end_clean(); // Borra cualquier error que haya saltado (como los <br> o <b>)
+    echo $respuesta;
     exit();
 }
 
-// Si no es ninguna de las anteriores y alguien intenta entrar por URL
-if(!isset($_POST['buscar_codigo']) && !isset($_POST['productos_venta'])){
-    session_start();
-    session_destroy();
-    header("Location: ../login/"); // O la ruta de tu login
+// 2. Finalizar venta (Botón Cobrar)
+if(isset($_POST['productos_venta'])) {
+    ob_start();
+    $respuesta = $insVenta->guardar_venta_controlador();
+    ob_end_clean();
+    echo $respuesta;
+    exit();
+}
+
+// Si no es ninguna, redirigir
+session_start();
+if(!isset($_SESSION['usuario_id'])){
+    header("Location: ../index.php");
     exit();
 }
